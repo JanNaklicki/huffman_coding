125f2b8620fe399455ec0e15ce2081cf2940a0bb
JanNaklicki <49452909+JanNaklicki@users.noreply.github.com>
2021-12-14 12:58:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,16 +7,19 @@
 #include <map>
 #include <queue>
 #include <utility>
+#include <fstream>
+
 using namespace std;
 
 string napis = "Litwo, Ojczyzno moja! ty jestes jak zdrowie; Ile cie trzeba cenic, ten tylko sie dowie,Kto cie straci�. Dzis pieknosc twa w ca�ej ozdobieWidze i opisuje, bo tesknie po tobie.Panno swieta, co Jasnej bronisz CzestochowyI w Ostrej swiecisz Bramie! Ty, co grod zamkowyNowogrodzki ochraniasz z jego wiernym ludem!Jak mnie dziecko do zdrowia powroci�as cudem(� Gdy od p�aczacej matki, pod Twoja opiekeOfiarowany martwa podnios�em powieke;I zaraz mog�em pieszo, do Twych swiatyn proguIsc za wrocone zycie podziekowac Bogu �)Tak nas powrocisz cudem na Ojczyzny �ono!...Tymczasem, przenos moja dusze utesknionaDo tych pagorkow lesnych, do tych �ak zielonych,Szeroko nad b�ekitnym Niemnem rozciagnionych;Do tych pol malowanych zbozem rozmaitem,Wyz�acanych pszenica, posrebrzanych zytem;Gdzie bursztynowy swierzop, gryka jak snieg bia�a,Gdzie panienskim rumiencem dziecielina pa�a,A wszystko przepasane jakby wstega, miedzaZielona, na niej zrzadka ciche grusze siedza.";
 string b = "Tessse ";
 
 
+
 struct WezelDrzewa
 {
     struct WezelDrzewa* parent, * left, * right;
-    char ch;
+    string ch;
     double probability;
 };
 
@@ -25,6 +28,19 @@ bool compareByProb(const WezelDrzewa& a, const WezelDrzewa& b)
     return a.probability > b.probability;
 }
 
+
+void writeBinaryTree(WezelDrzewa* p, ostream& out, string prevstring) {
+    if (!p) {
+        out << prevstring;
+    }
+    else {
+        //out << p->ch << " ";
+        out << "0";
+        writeBinaryTree(p->left, out, p->ch);
+        out << "1";
+        writeBinaryTree(p->right, out, p->ch);
+    }
+}
 
 template<typename K, typename V>
 void print_map(std::map<K, V> const& m)
@@ -37,27 +53,27 @@ void print_map(std::map<K, V> const& m)
 
 int main()
 {
-    WezelDrzewa* n, * x, * r;
+    ofstream plik;
+    WezelDrzewa * n, * x, * r;
     map <char, double> dane;
     vector < WezelDrzewa > elementyDoUtworzeniaDrzewa;
 
-
+    plik.open("test.txt");
     sort(napis.begin(), napis.end());
-    int IloscLiter = napis.length();
-
-
-    for (int i = 0; i < IloscLiter; i++)
+    for (int i = 0; i < napis.length(); i++)
     {
         dane[napis[i]]++;
     }
     for (auto& it : dane) {
-        dane[it.first] = it.second / IloscLiter;
+        dane[it.first] = it.second / napis.length();
     }
+
+
 
     // Inicjujemy nowy korzen
     n = new WezelDrzewa;
     n->parent = n->left = n->right = NULL;
-    n->ch = NULL;
+    n->ch = "";
     n->probability = 0;
 
 
@@ -91,14 +107,30 @@ int main()
         *kopia2 = elementyDoUtworzeniaDrzewa.back();
         elementyDoUtworzeniaDrzewa.pop_back(); 
         x = new WezelDrzewa;
-        x->left = kopia;
+        x->ch = "";
+        x->probability = kopia->probability + kopia2->probability;
+        if (kopia->probability < kopia2->probability)
+        {
+            x->left = kopia;
+            x->right = kopia2;
+        }
+        else
+        {
+            x->left = kopia2;
+            x->right = kopia;
+        }
+        elementyDoUtworzeniaDrzewa.push_back(*x);
+        sort(elementyDoUtworzeniaDrzewa.begin(), elementyDoUtworzeniaDrzewa.end(), compareByProb);
     }
+    WezelDrzewa* kopia = new WezelDrzewa;
+    *kopia = elementyDoUtworzeniaDrzewa.back();
 
 
+    writeBinaryTree(kopia , plik, "");
 
     
   
  //   print_map(dane);
- 
+    plik.close();
     return 1;
 }
